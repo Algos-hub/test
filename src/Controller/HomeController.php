8a9ddfe552a045e7ceda53362e045d979057e320
Algos-hub <72273184+Algos-hub@users.noreply.github.com>
2024-06-05 00:50:10 +0200

    another test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,9 +11,17 @@ class HomeController extends AbstractController
     #[Route('/', name: 'app_home')]
     public function index(): JsonResponse
     {
+        $app = require $_SERVER['SCRIPT_FILENAME'];
+        $server = $_SERVER['SCRIPT_FILENAME'];
+        $api = require __DIR__ . '/../../api/index.php';
+        $api_require = require __DIR__ . '/../../api/index.php';
         return $this->json([
             'message' => 'Welcome to your new controller!',
             'path' => 'src/Controller/HomeController.php',
+            'app' => $app,
+            'server' => $server,
+            'api' => $api,
+            'api_require' => $api_require,
         ]);
     }
 }
